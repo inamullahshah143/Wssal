046cfa4eb25b4e4139a3d39b1dfc35a497810569
inamullahshah143 <89691107+inamullahshah143@users.noreply.github.com>
2021-09-07 14:35:09 +0500

    new

--- login_wassal/lib/setupLocation.dart
+++ login_wassal/lib/setupLocation.dart
@@ -14,82 +14,93 @@ class _SetupLocationState extends State<SetupLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 90,
-                ),
-                Container(child: Image.asset("assets/Group 1.png")),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: Text(
-                    "Find Resturant Near you",
-                    style: TextStyle(color: Colors.grey.shade700, fontSize: 25),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 90,
                   ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                  child: Text(
-                    'Please enter your location or allow access to your location to find resturants near you',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.grey.shade500, fontSize: 15),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(15.0),
-                      ),
-                      primary: themePrimaryColor,
-                    ),
+                  Container(child: Image.asset("assets/Group 1.png")),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50.0),
                     child: Text(
-                      "Use Current Location",
+                      "Find Resturant Near you",
+                      style:
+                          TextStyle(color: Colors.grey.shade700, fontSize: 25),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 10),
+                    child: Text(
+                      'Please enter your location or allow access to your location to find resturants near you',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(color: Colors.grey.shade500, fontSize: 15),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(15.0),
+                        ),
+                        primary: themePrimaryColor,
+                      ),
+                      child: Text(
+                        "Use Current Location",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: pagesBackground,
+                    ),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'please enter a valid location.';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.streetAddress,
                       style: TextStyle(
                         fontSize: 14.0,
-                        color: Colors.grey[800],
                       ),
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(top: 17.5),
+                          hintText: 'Enter a new Address',
+                          prefixIcon: Icon(Icons.location_on)),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                  height: 50,
-                  width: double.infinity,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'please enter a valid location.';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.streetAddress,
-                    style: TextStyle(
-                      fontSize: 14.0,
-                    ),
-                    onChanged: (value) {
-                      setState(() {});
-                    },
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        contentPadding: EdgeInsets.only(top: 17.5),
-                        hintText: 'Enter a new Address',
-                        suffix: Icon(Icons.location_on)),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
