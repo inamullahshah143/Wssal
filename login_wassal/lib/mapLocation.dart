1953680ac1276bae3dbe1803f2ba76d037961adc
Mubeen Ul haq <82878766+mubi5177@users.noreply.github.com>
2021-09-08 15:26:23 +0500

    latest

--- login_wassal/lib/mapLocation.dart
+++ login_wassal/lib/mapLocation.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:wassal_customer/const.dart';
+// ignore: must_be_immutable
 class MapLoacation extends StatefulWidget {
-  const MapLoacation({Key key}) : super(key: key);
-
+  Position currentPosition;
+  MapLoacation({
+    @required this.currentPosition,
+  });
   @override
-  _MapLoacationState createState() => _MapLoacationState();
+  _MapLoacationState createState() =>
+      _MapLoacationState(currentPosition: currentPosition);
 }
-
 class _MapLoacationState extends State<MapLoacation> {
+  Position currentPosition;
+  _MapLoacationState({
+    @required this.currentPosition,
+  });
   GoogleMapController myController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
   void _onMapCreated(GoogleMapController controller) {
     myController = controller;
   }
-
   @override
   Widget build(BuildContext context) {
+    final LatLng _center =
+        LatLng(currentPosition.latitude, currentPosition.longitude);
     return Container(
+      clipBehavior: Clip.hardEdge,
       height: MediaQuery.of(context).size.height / 1.2,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -30,6 +37,13 @@ class _MapLoacationState extends State<MapLoacation> {
       child: Stack(
         children: <Widget>[
           GoogleMap(
+            zoomGesturesEnabled: true,
+            padding: EdgeInsets.all(10),
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            zoomControlsEnabled: false,
+            scrollGesturesEnabled: true,
+            compassEnabled: true,
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
               target: _center,
@@ -37,14 +51,97 @@ class _MapLoacationState extends State<MapLoacation> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(16.0),
             child: Align(
-              alignment: Alignment.topRight,
-              child: FloatingActionButton(
-                onPressed: () => print('button pressed'),
-                materialTapTargetSize: MaterialTapTargetSize.padded,
-                backgroundColor: Colors.green,
-                child: const Icon(Icons.map, size: 36.0),
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 10,
+                width: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey[600],
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey[300],
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 0.5),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey[300],
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 0.5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      height: 50,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: pagesBackground,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          counterText: "",
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(top: 17.5),
+                          hintText: 'Search for Location',
+                          prefixIcon: Icon(Icons.location_on),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(15.0),
+                          ),
+                          primary: themePrimaryColor,
+                        ),
+                        child: Text(
+                          "Setup Your Location",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
